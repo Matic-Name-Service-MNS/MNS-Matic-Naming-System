f068bd4549c378aa596b438e604cc74696712d02
Tisi22 <teresa@ngmi.ai>
2023-01-13 16:29:55 +0100

    update

--- contracts/registrar/PolygonRegistrarController.sol
+++ contracts/registrar/PolygonRegistrarController.sol
@@ -65,9 +65,9 @@ contract PolygonRegistrarController is Ownable {
   /**
   * @dev Makes a new commitment
   */
-  function makeCommitmentWithConfig(string memory name, bytes32 secret, address resolver) public view returns(bytes32) {
+  function makeCommitmentWithConfig(string memory name, bytes32 secret, address adr, address resolver) public pure returns(bytes32) {
     bytes32 label = keccak256(bytes(name));
-    return keccak256(abi.encodePacked(label, msg.sender, resolver, secret));
+    return keccak256(abi.encodePacked(label, adr, resolver, secret));
   }
 
   /**
@@ -76,7 +76,7 @@ contract PolygonRegistrarController is Ownable {
   function registerDomain(string memory name, bytes32 secret, address resolver) public payable {
     require(resolver != address(0), "Need a resolver contract");
 
-    bytes32 commitment = makeCommitmentWithConfig(name, secret, resolver);
+    bytes32 commitment = makeCommitmentWithConfig(name, secret, msg.sender, resolver);
     uint256 cost =  _consumeCommitment(name, commitment);
 
     bytes32 label = keccak256(bytes(name));
